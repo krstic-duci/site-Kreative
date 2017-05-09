3bee386d7358b57e288804117d8a5cc77482efaf
Dusan Krstic <krstic.duci@gmail.com>
2017-05-09 23:44:13 +0200

    few lines of code changed

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,12 @@
 $(function () {
-	
+	/*----------  put footer navbar on new line on < 701px  ----------*/
+	$( window ).resize(function() {
+  		if($(window).width() <= 836) {
+  			$('.custom-col-sm').addClass('col-sm-6');
+ 		}else {
+  			$('.custom-col-sm').removeClass('col-sm-6');
+  		}	
+	});
 });
 
 
