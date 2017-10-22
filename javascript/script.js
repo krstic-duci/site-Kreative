44d3042e87ebb14b86a4689dc6236a1cd0980ada
Dusan Krstic <krstic.duci@gmail.com>
2017-10-23 01:25:47 +0200

    Refactored code

--- javascript/script.js
+++ javascript/script.js
@@ -1,173 +1,225 @@
 $(function () {
 
-    function smoothScroolOnClick () {
-      $('a[href*="#"]').click(function(event) {
+/*=====================================
+=            Smooth scroll            =
+=====================================*/
   
-        // On-page links
-        if (location.pathname.replace(/^\//, '') == 
-          this.pathname.replace(/^\//, '') && location.hostname == 
-          this.hostname) {
+  function smoothScroolOnClick() {
+    $('a[href*="#"]').click(function(event) {
 
-          // Figure out element to scroll to
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // On-page links
+      if (location.pathname.replace(/^\//, '') == 
+        this.pathname.replace(/^\//, '') && location.hostname == 
+        this.hostname) {
 
-          // Does a scroll target exist?
-          if (target.length) {
-              // Only prevent default if animation is actually gonna happen
-              event.preventDefault();
-              $('html, body').animate({
-                  scrollTop: target.offset().top
-              }, 300);
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 300);
           }
         }
-      });
-    }
+    });
+  }
+  smoothScroolOnClick();
+  
+/*=====  End of Smooth scroll  ======*/
 
-    function modalLoader () {
 
-      $('.custom-col-sm-2').on('click', function(){
+/*===========================================
+=            Modal Loader Images            =
+===========================================*/
+  
+  function modalLoader() {
 
-        /*----------  for taking attr from img src & alt  ----------*/
-        var imgSrc = $(this).find('img').attr('src');
-        var modalsSrc = imgSrc.slice(0,18) + '/modals/m-' + imgSrc.slice(19);
-        var altSrc = $(this).find('img').attr('alt');
+    $('.custom-col-sm-2').on('click', function(){
 
-        /*----------  for taking value & insert in modal body  ----------*/
-        var headingAndBrand = imgSrc.slice(19);
-        var textInHeading6 = '';
-        var spanTextInsidePara = '';
-        var paraTextModalImg = 'Proin gravida nibh vel velit auctor'+
-        'aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi '+
-        'elit consequat ipsum, nec sagittis sem nibh id elit.';
+      /*----------  for taking attr from img src & alt  ----------*/
+      var imgSrc = $(this).find('img').attr('src');
+      var modalsSrc = imgSrc.slice(0,18) + '/modals/m-' + imgSrc.slice(19);
+      var altSrc = $(this).find('img').attr('alt');
 
-        switch (headingAndBrand) {
-          case 'cosmic-sneakers.jpg':
-            textInHeading6 = 'The Cosmics Sneakers';
-            spanTextInsidePara = 'Branding, Web Design';
-            break;
-          case 'milk-splash.jpg':
-             textInHeading6 = 'Milk Splash';
-             spanTextInsidePara = 'Branding, Web Design';
-             break;
-          case 'eve.jpg':
-            textInHeading6 = 'Eve';
-            spanTextInsidePara = 'Photography, Branding';
-            break;
-          case 'sneaker-splash.jpg':
-             textInHeading6 = 'Sneaker Splash';
-             spanTextInsidePara = 'Photography, Branding';
-             break;
-          case 'judah.jpg':
-            textInHeading6 = 'Judah';
-            spanTextInsidePara = 'Photography, Web Design';
-            break;
-          case 'vector-flower.jpg':
-            textInHeading6 = 'Vector Flower';
-            spanTextInsidePara = 'Branding, Web Design';
-            break;
-          case 'clock.jpg':
-            textInHeading6 = 'Clock';
-            spanTextInsidePara = 'Branding, Web Design';
-            break;
-          case 'fields.jpg':
-            textInHeading6 = 'Fields';
-            spanTextInsidePara = 'Photography';
-            break;
-        }
+      /*----------  for taking value & insert in modal body  ----------*/
+      var headingAndBrand = imgSrc.slice(19);
+      var textInHeading6 = '';
+      var spanTextInsidePara = '';
+      var paraTextModalImg = 'Proin gravida nibh vel velit auctor'+
+      'aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi '+
+      'elit consequat ipsum, nec sagittis sem nibh id elit.';
 
-        /*----------  add value to modal  ----------*/
-        $('.change-pic-in-modal').html(
+      switch (headingAndBrand) {
+        case 'cosmic-sneakers.jpg':
+        textInHeading6 = 'The Cosmics Sneakers';
+        spanTextInsidePara = 'Branding, Web Design';
+        break;
+        case 'milk-splash.jpg':
+        textInHeading6 = 'Milk Splash';
+        spanTextInsidePara = 'Branding, Web Design';
+        break;
+        case 'eve.jpg':
+        textInHeading6 = 'Eve';
+        spanTextInsidePara = 'Photography, Branding';
+        break;
+        case 'sneaker-splash.jpg':
+        textInHeading6 = 'Sneaker Splash';
+        spanTextInsidePara = 'Photography, Branding';
+        break;
+        case 'judah.jpg':
+        textInHeading6 = 'Judah';
+        spanTextInsidePara = 'Photography, Web Design';
+        break;
+        case 'vector-flower.jpg':
+        textInHeading6 = 'Vector Flower';
+        spanTextInsidePara = 'Branding, Web Design';
+        break;
+        case 'clock.jpg':
+        textInHeading6 = 'Clock';
+        spanTextInsidePara = 'Branding, Web Design';
+        break;
+        case 'fields.jpg':
+        textInHeading6 = 'Fields';
+        spanTextInsidePara = 'Photography';
+        break;
+      }
+
+      /*----------  add value to modal  ----------*/
+      $('.change-pic-in-modal').html(
         '<img src="' + modalsSrc + '" alt="' + altSrc + '">'+
         '<h6>'+ textInHeading6 +  '</h6>'+
         '<p class="para-text-modal-img">'+ paraTextModalImg +'</p>'+
         '<p>'+ 
-          '<i class="fa fa-tag" aria-hidden="true"></i>'+ 
-          '<span class="tag-text-modal-img">'+spanTextInsidePara+'</span>'+
+        '<i class="fa fa-tag" aria-hidden="true"></i>'+ 
+        '<span class="tag-text-modal-img">'+spanTextInsidePara+'</span>'+
         '</p>');
-      });
-    }
+    });
+  }
+  modalLoader();
 
+/*=====  End of Modal Loader Images  ======*/
+  
 
-    function loadGeoLocation () {
-    
-      // Loading Gmaps for Philippines
-      var Gmap = new GMaps({
-        div: '#map',
-        lat: 14.553304,
-        lng: 121.051657,
-        zoom: 15
-      });
+/*======================================
+=            Load GMaps API            =
+======================================*/
+  
+  function loadGeoLocation() {
 
-      var m = Gmap.addMarker({
-        lat: 14.553304,
-        lng: 121.051657,
-        infoWindow: {
-          content: '<p>32nd St of South Cembo District, Philippines</p>'
-        }
-      });
+    // Loading Gmaps for Philippines
+    var Gmap = new GMaps({
+      div: '#map',
+      lat: 14.553304,
+      lng: 121.051657,
+      zoom: 15
+    });
 
-      if(Gmap.map) {
-        // Disabling mouse wheel scroll zooming
-        Gmap.map.setOptions({ scrollwheel: false });
+    var m = Gmap.addMarker({
+      lat: 14.553304,
+      lng: 121.051657,
+      infoWindow: {
+        content: '<p>32nd St of South Cembo District, Philippines</p>'
       }
+    });
+
+    if(Gmap.map) {
+      // Disabling mouse wheel scroll zooming
+      Gmap.map.setOptions({ scrollwheel: false });
     }
+  }
+  loadGeoLocation();
+
+/*=====  End of Load GMaps API  ======*/
 
 
-    function spinImage () {
+/*=================================================
+=            Load image on form submit            =
+=================================================*/
 
-      //When click on submit button image loading appears
-      $('.button-submit').click(function() {
-          $('.fa-spinner').addClass('fa-spin').css('display', 'inline');
-      });
-    }
+  function spinImage() {
+    //When click on submit button image loading appears
+    $('.button-submit').click(function() {
+      $('.fa-spinner').addClass('fa-spin').css('display', 'inline');
+    });
+  }
+  spinImage();
+/*=====  End of Load Image on Form submit  ======*/
 
-    function changeHeaderProp () {
 
-      // On mouse click on arrow change displaying text
-      $('.left-arrow-wrapper, .right-arrow-wrapper').click(function () {
-        $('.first-text-to-swap').toggle('slow', 'swing');
-        $('.second-text-to-swap').toggle('slow', 'swing');
-      });
+/*===================================================
+=            Load text swaping in header            =
+====================================================*/
 
-      // On mouse enter/leave over header show controls
-      $('.introduction-part').mouseenter(function() {
-        $('.control-text').show();
-      });
-      $('.introduction-part').mouseleave(function() {
-        $('.control-text').hide();
-      });
-    }
+  function changeHeaderProp() {
 
-	/*----------  put cols & paras on new lines ----------*/
-	$(window).resize(function() {
+    // On mouse click on arrow change displaying text
+    $('.left-arrow-wrapper, .right-arrow-wrapper').click(function () {
+      $('.first-text-to-swap').toggle('slow', 'swing');
+      $('.second-text-to-swap').toggle('slow', 'swing');
+    });
 
-		if ($(window).width() <= 819) {
+    // On mouse enter/leave over header show controls
+    $('.introduction-part').mouseenter(function() {
+      $('.control-text').show();
+    });
+    $('.introduction-part').mouseleave(function() {
+      $('.control-text').hide();
+    });
+  }
+  changeHeaderProp();
+/*=====  End of Load text swaping in header  ======*/
 
-			/*----------  put Services section on new lines ----------*/
-			$('.custom-col-services').removeClass('col-sm-8').addClass('col-sm-12');
-			$('.custom-col-sm').removeClass('col-sm-4').addClass('col-sm-6');
 
-			/*----------  put Portfolio section on new lines ----------*/
-			$('.custom-col-sm-2').removeClass('col-sm-3').addClass('col-sm-4');
+/*=======================================
+=            Add breakpoints            =
+=======================================*/
 
-			/*----------  put About Us section second para on new lines ----------*/
-			$('.custom-col-sm-3').removeClass('col-sm-9').addClass('col-sm-12');
+  function breakpoints() {
+    var $width = $(window).width();
 
-			/*----------  put About Us column para on new lines ----------*/
-			$('.custom-col-sm-4').removeClass('col-sm-3').addClass('col-sm-6');
-		} else {
+    if($width <= 819) {
+      /*----------  put Services section on new lines ----------*/
+      $('.custom-col-services').removeClass('col-sm-8').addClass('col-sm-12');
+      $('.custom-col-sm').removeClass('col-sm-4').addClass('col-sm-6');
 
-			/*----------  put col-sm, para & margin to default value----------*/
-			$('.custom-col-services').removeClass('col-sm-12').addClass('col-sm-8');
-			$('.custom-col-sm').removeClass('col-sm-6').addClass('col-sm-4');
-			$('.custom-col-sm-2').removeClass('col-sm-4').addClass('col-sm-3');
-			$('.custom-col-sm-3').removeClass('col-sm-12').addClass('col-sm-9');
-			$('.custom-col-sm-4').removeClass('col-sm-6').addClass('col-sm-3');
-		}
+      /*----------  put Portfolio section on new lines ----------*/
+      $('.custom-col-sm-2').removeClass('col-sm-3').addClass('col-sm-4');
 
+      /*----------  put About Us section second para on new lines ----------*/
+      $('.custom-col-sm-3').removeClass('col-sm-9').addClass('col-sm-12');
+
+      /*----------  put About Us column para on new lines ----------*/
+      $('.custom-col-sm-4').removeClass('col-sm-3').addClass('col-sm-6');
+     } else {
+
+      /*----------  put col-sm, para & margin to default value----------*/
+      $('.custom-col-services').removeClass('col-sm-12').addClass('col-sm-8');
+      $('.custom-col-sm').removeClass('col-sm-6').addClass('col-sm-4');
+      $('.custom-col-sm-2').removeClass('col-sm-4').addClass('col-sm-3');
+      $('.custom-col-sm-3').removeClass('col-sm-12').addClass('col-sm-9');
+      $('.custom-col-sm-4').removeClass('col-sm-6').addClass('col-sm-3');
+     }
+  }
+  breakpoints();
+
+
+  
+
+/*=====  End of Add breakpoints  ======*/
+  
+/*=======================================================
+=            Add left/right arrows in header            =
+=======================================================*/
+
+  function leftRightArrow() {
+    var $screenWidth = $(window).width();
+  
     // When screen size iz <=1024px show control arrows in header
-    if ($(window).width() <= 1007) {
+    if ($screenWidth <= 1007) {
       $('.control-text').show();
       $('.introduction-part').mouseleave(function() {
         $('.control-text').show();
@@ -178,20 +230,18 @@ $(function () {
         $('.control-text').hide();
       });
     }
-	});
+  }
+  leftRightArrow();
+/*=====  End of Add left/right arrows in header  ======*/
 
-  /*----------  for smooth scrolling  ----------*/
-  smoothScroolOnClick();
+/*========================================================================
+=            Load fnc breakpoints and leftRightArrow on rezie            =
+=========================================================================*/
 
-  /*----------  for loading modal pics  ----------*/
-  modalLoader();
+  $(window).resize(function() {
+    breakpoints();
+    leftRightArrow();
+  });
 
-  /*----------  for loading Google geolocation  ----------*/
-  loadGeoLocation();
-
-  /*----- for adding image when click on submit btn -----*/
-  spinImage();
-
-  /*----- for changing text in header -----*/
-  changeHeaderProp();
+/*=====  End of Load fnc breakpoints and leftRightArrow on rezie  ======*/
 });
